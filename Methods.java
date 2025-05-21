e4c39a17f0c6c5695d81818ef79bb24b744f2145
Dreamless07 <saadkhan7903@gmail.com>
2025-05-21 10:35:36 +0530

    testing

--- Methods.java
+++ Methods.java
@@ -1,5 +1,5 @@
 public class Methods {
     public static void main(String[] args) {
-        System.out.println("This is testing code");
+        System.out.println("This is testing code for java");
     }
 }
