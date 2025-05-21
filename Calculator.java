1f12567f9488e7c319f8d6e5c6f9529d06fb838d
Dreamless07 <saadkhan7903@gmail.com>
2025-05-21 11:26:06 +0530

    calculator using multiple methods in smae class 21/05/2025

---
+++ Calculator.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+class calci{
+    public void addition (int a, int b){
+        System.out.println((a+b));
+    }
+    public void subtraction (int a, int b){
+        System.out.println((a-b));
+    }
+    public void multiplication (int a, int b){
+        System.out.println((a*b));
+    }
+    public void division (int a, int b){
+        System.out.println((a/b));
+    }
+    public void modulo (int a, int b){
+        System.out.println((a%b));
+    }
+}
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter value of a and b");
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        calci obj=new calci();
+        obj.addition(a,b);
+        obj.subtraction(a,b);
+        obj.multiplication(a,b);
+        obj.division(a,b);
+        obj.modulo(a,b);
+
+
+
+    }
+}
