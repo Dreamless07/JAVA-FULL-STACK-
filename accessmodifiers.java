bcb9d6ffa765106ad549ce147f32fc1789992495
Dreamless07 <saadkhan7903@gmail.com>
2025-05-22 11:05:31 +0530

    private access modifier

---
+++ accessmodifiers.java
@@ -0,0 +1,15 @@
+class noaccess {//class
+    private int num = 10;
+    private void display() {//method
+        System.out.println("accessed the method from another method");
+    }
+    void accessdisplayfunc () {//added one more method inside the class to access the private method
+        display();//calls it
+    }
+}
+public class accessmodifiers {
+    public static void main(String[] args) {
+        noaccess obj = new noaccess();//create object
+        obj.accessdisplayfunc();//call the method
+    }
+}
