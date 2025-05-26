a7108fa466b9f85a683c405bb3473bc78f236331
Dreamless07 <saadkhan7903@gmail.com>
2025-05-26 11:04:05 +0530

    compile time polymorphism using overloading

---
+++ CompileTime_polymorphism.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+class Info{
+    void Student(String name,String branch){
+        System.out.println("The name of the STUDENT is "+name+" and the BRANCH is "+branch);
+    }
+    void Student(String name,String branch,String address){
+        System.out.println("The name of the STUDENT is "+name+", the BRANCH is "+branch+" and the address is "+address);
+    }
+}
+
+
+public class CompileTime_polymorphism {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Info obnew=new Info();
+        System.out.println("Enter the Student name and mention address if availabe");
+        String name=sc.nextLine();
+        String branch=sc.nextLine();
+        String address=sc.nextLine();
+        obnew.Student(name, branch);
+        obnew.Student(name, branch, address);
+    }
+}
