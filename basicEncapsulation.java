20bab476d0eab1a25b3636c247e183031e4c6323
Dreamless07 <saadkhan7903@gmail.com>
2025-05-27 11:10:07 +0530

    encapsulation(set and get methods)program

---
+++ basicEncapsulation.java
@@ -0,0 +1,34 @@
+//Encapsulation
+class aboutperson {//class
+    private String name = "Saad";
+    private int age = 22;//private variables
+
+    public String getName() {
+        return name;//to access private vars we use get() method /can also use a public method(performing encapsulation)
+    }
+
+    public void setName(String name1) {
+        this.name = name1;//to set a different value to the var we use set() method
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age1) {
+        this.age = age1;
+    }
+}
+
+public class basicEncapsulation {
+    public static void main(String[] args) {
+        aboutperson OBJ= new aboutperson();
+        OBJ.getName();//calling the getter method to access private(hidden) details
+        OBJ.getAge();
+        System.out.println("Name: "+OBJ.getName()+"\nAge: "+OBJ.getAge());//print
+        OBJ.setName("Aftab");//calling setter method to change the private value
+        OBJ.setAge(23);
+        System.out.println("Name: "+OBJ.getName()+"\nAge: "+OBJ.getAge());//print it again by calling get with updated value now
+
+    }
+}
