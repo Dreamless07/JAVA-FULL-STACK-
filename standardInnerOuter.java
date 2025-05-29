f80a80adad18ba9d0e699635c1bc654b02f6fc8e
Dreamless07 <saadkhan7903@gmail.com>
2025-05-29 12:16:50 +0530

    standard inner and outer classes with arguments program

--- standardInnerOuter.java
+++ standardInnerOuter.java
@@ -1,12 +1,16 @@
 class outerclassnew{//outer
     private class innerclass{//private inner
-        void newmmethod(String nameofperson, int ageofperson){//a method with arguments
-            System.out.println("Name of the person is "+nameofperson+" and the age is "+ageofperson);//cannot access outside outer class with object
+        void emp(String empname, int empid){//a method with arguments
+            System.out.println("Name of the employee is "+empname+" and the ID is "+empid);//cannot access outside outer class with object
+        }
+        void dept(String nameofdept, int deptid){
+            System.out.println("Name of the department is "+nameofdept+" and the ID is "+deptid);
         }
     }
     void anotherclassnew(){//to access inner class we use another method inside outer class
         innerclass objinner=new innerclass();//create object to access inner class now
-        objinner.newmmethod("Saad",22);
+        objinner.emp("Saad",22);
+        objinner.dept("Full Stack Engineer",2215);
     }
 }
 public class standardInnerOuter {
