421d8fa59372f3e602a77c14192d48e02cf78d9c
Dreamless07 <saadkhan7903@gmail.com>
2025-06-02 11:01:07 +0530

    Arrays

---
+++ Arrays/src/characterArray.java
@@ -0,0 +1,8 @@
+public class characterArray {
+    public static void main(String[] args) {
+        char name[] = {'S', 'A', 'A', 'D'};
+        for (int i = 0; i < name.length; i++) {//with for loop increment till the last index of array
+            System.out.println(name[i]);
+        }
+    }
+}
