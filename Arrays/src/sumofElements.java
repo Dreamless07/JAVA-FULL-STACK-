53344e4af23694b216028901eea3a8e27fad3b64
Dreamless07 <saadkhan7903@gmail.com>
2025-06-02 11:08:09 +0530

    Arrays

---
+++ Arrays/src/sumofElements.java
@@ -0,0 +1,10 @@
+public class sumofElements {
+    public static void main(String[] args) {
+        int a[]={1,7,7,8,4,5,9};
+        int sum=0;
+        for(int i=0;i<a.length;i++){
+            sum=sum+a[i];
+            System.out.println(sum);
+        }
+    }
+}
