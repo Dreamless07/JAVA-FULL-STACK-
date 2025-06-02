cb0a54ff8f2eadc68ac56afcb68f2572a7bb767f
Dreamless07 <saadkhan7903@gmail.com>
2025-06-02 10:55:37 +0530

    Arrays first program

--- Arrays/src/first.java
+++ Arrays/src/first.java
@@ -1,6 +1,7 @@
 public class first {
     public static void main(String[] args) {
         int array[]={3,8,2,5,9};//define array
+        array[2]=25;//change an index value
         for(int i=0; i<array.length; i++){//with for loop increment till the last index of array
             System.out.println(array[i]);
         }
