9397cbe53c06830f7bab72d83dc144a9877d6514
Dreamless07 <saadkhan7903@gmail.com>
2025-06-03 10:46:32 +0530

    2D array basic

--- Arrays/src/basicMultiDArray.java
+++ Arrays/src/basicMultiDArray.java
@@ -1,7 +1,7 @@
 public class basicMultiDArray {
     public static void main(String[] args) {
         int[][] matrix={{1,2,3},{4,5,6},{7,8,9}};//2D array declaration and definition
-        System.out.println("The 2D array elements are");
+        System.out.println("The 2D array elements are:");
         for(int i=0;i<matrix.length;i++){//for row
             for(int j=0;j<matrix.length;j++){//for coloumn
                 System.out.print(+matrix[i][j]+" ");//for spacing in between
