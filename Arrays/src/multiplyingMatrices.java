3312a971bafdac32eb118b0c341eb6bdca23cadf
Dreamless07 <saadkhan7903@gmail.com>
2025-06-09 10:37:07 +0530

    matrix multiplication updated

--- Arrays/src/multiplyingMatrices.java
+++ Arrays/src/multiplyingMatrices.java
@@ -12,30 +12,30 @@ public class multiplyingMatrices {
             System.out.println("Matrix multiplication is not possible because Columns of A must be equal to rows of B.");
             return;
         }
-        int[][] matrixA = new int[rowA][colA];
+        int[][] matrixA = new int[rowA][colA];//initialization of matrices with respective rows and columns
         int[][] matrixB = new int[rowB][colB];
         int[][] matrixC = new int[rowA][colB];
         System.out.println("Enter elements of Matrix A:");
-        for (int i = 0; i < rowA; i++) {
+        for (int i = 0; i < rowA; i++) {//input
             for (int j = 0; j < colA; j++) {
                 matrixA[i][j] = sc.nextInt();
             }
         }
         System.out.println("Enter elements of Matrix B:");
-        for (int i = 0; i < rowB; i++) {
+        for (int i = 0; i < rowB; i++) {//input
             for (int j = 0; j < colB; j++) {
                 matrixB[i][j] = sc.nextInt();
             }
         }
-        for (int i = 0; i < rowA; i++) {
+        for (int i = 0; i < rowA; i++) {//multiplication logic
             for (int j = 0; j < colB; j++) {
                 for (int k = 0; k < colA; k++) {
-                    matrixC[i][j] += matrixA[i][k] * matrixB[k][j];
+                    matrixC[i][j] += matrixA[i][k] * matrixB[k][j];//multiply rows of A and col of B and store in C
                 }
             }
         }
         System.out.println("Result of matrix multiplication:");
-        for (int i = 0; i < rowA; i++) {
+        for (int i = 0; i < rowA; i++) {//print resultant matrix
             for (int j = 0; j < colB; j++) {
                 System.out.print(matrixC[i][j] + " ");
             }
