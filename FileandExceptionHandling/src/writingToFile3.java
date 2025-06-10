0ce67b457987dcc0130ecad8ff62787db864e280
Dreamless07 <saadkhan7903@gmail.com>
2025-06-10 13:09:35 +0530

    3rd way for writing into a file

---
+++ FileandExceptionHandling/src/writingToFile3.java
@@ -0,0 +1,11 @@
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class writingToFile3 {
+    public static void main(String[] args) throws Exception{
+        String forwrite="testingforwriting.txt";
+        String newcontent="Hello this file is written into manually by the third way";
+        Files.write(Paths.get(forwrite),newcontent.getBytes());
+        System.out.println("Content successfully inserted");
+    }
+}
