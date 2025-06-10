f87f7a88781a5ff0bea2acad020e80c8769f8830
Dreamless07 <saadkhan7903@gmail.com>
2025-06-10 12:39:28 +0530

    updated

--- FileandExceptionHandling/src/creatingFile.java
+++ FileandExceptionHandling/src/creatingFile.java
@@ -1,7 +1,7 @@
 import java.io.File;//inbuilt libraries
 public class creatingFile {
     public static void main(String[] args) throws Exception{
-        String a="testingnew.abc";//new file
+        String a="testingforwriting.txt";//new file
         File newfile=new File(a);//file object
         if(newfile.createNewFile()){//if file created then
             System.out.println("File created successfully");
