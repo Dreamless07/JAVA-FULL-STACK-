925c1eaaa9211aa133ffbd0b7826e7aae7071f07
Dreamless07 <saadkhan7903@gmail.com>
2025-06-12 10:59:32 +0530

    Checked and unchecked exceptions

---
+++ FileandExceptionHandling/src/checkedandUncheckedException.java
@@ -0,0 +1,12 @@
+//exceptions
+import java.io.File;
+import java.io.FileReader;
+public class checkedandUncheckedException {
+    public static void main(String[] args) {
+        File fileobject = new File("abc.txt");//file not there
+        FileReader readobject=new FileReader(fileobject);//file missing or not found exception(checked/compile time)
+        int arraystore[]={1,2,3,4};//array size only 4
+        System.out.println(arraystore[5]);//array index out of bound exception(unchecked/runtime)
+    }
+
+}
