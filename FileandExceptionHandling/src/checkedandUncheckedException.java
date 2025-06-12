fa70f6837c2b90ecffecce854a6fbafc80a35000
Dreamless07 <saadkhan7903@gmail.com>
2025-06-12 11:00:39 +0530

    Checked and unchecked exceptions

--- FileandExceptionHandling/src/checkedandUncheckedException.java
+++ FileandExceptionHandling/src/checkedandUncheckedException.java
@@ -2,11 +2,10 @@
 import java.io.File;
 import java.io.FileReader;
 public class checkedandUncheckedException {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         File fileobject = new File("abc.txt");//file not there
         FileReader readobject=new FileReader(fileobject);//file missing or not found exception(checked/compile time)
         int arraystore[]={1,2,3,4};//array size only 4
         System.out.println(arraystore[5]);//array index out of bound exception(unchecked/runtime)
     }
-
 }
