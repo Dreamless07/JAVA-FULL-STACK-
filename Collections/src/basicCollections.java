7a7adf35ce25306227d940720cef0d224eb96e20
Dreamless07 <saadkhan7903@gmail.com>
2025-06-16 12:07:31 +0530

    basic collections program

--- Collections/src/basicCollections.java
+++ Collections/src/basicCollections.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.Collection;//inbuilt libraries
 public class basicCollections {
     public static void main(String[] args) {
-        Collection<String> name=new ArrayList<>();//Since we cannot create object directly from interface we can use inbuilt class ie Arraylist etc.
+        Collection<String> name=new ArrayList<>();//Since we cannot create object directly from interface we use inbuilt class ie Arraylist etc.
         name.add("Bob");
         name.add("Harry");
         name.add("Burton");
