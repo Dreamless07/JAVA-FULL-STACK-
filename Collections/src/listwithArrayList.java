6c8a22d3989a5b7f441fb94837ba13b0749ddaa7
Dreamless07 <saadkhan7903@gmail.com>
2025-06-16 12:14:56 +0530

    using List interface with Arraylist inbuilt class

---
+++ Collections/src/listwithArrayList.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;//inbuilt libraries
+public class listwithArrayList {
+    public static void main(String[] args) {
+        List<Integer> newList= new ArrayList<>();//list object with inbuilt class
+        newList.add(10);
+        newList.add(50);
+        newList.add(40);
+        newList.add(60);//add first
+        System.out.println(newList.size());//check size
+        System.out.println(newList);
+        System.out.println(newList.get(3));//get element
+        System.out.println(newList);
+        newList.set(1,70);//change element
+        System.out.println(newList);
+        newList.remove(2);//remove element
+        System.out.println(newList);
+        System.out.println(newList.contains(10));//check for element
+    }
+}
