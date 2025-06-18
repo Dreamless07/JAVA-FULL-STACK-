ce167531826be2fa50e796cb22176f27aa2d7bea
Dreamless07 <saadkhan7903@gmail.com>
2025-06-18 10:52:21 +0530

    using Priority Queue interface by using inbuilt class as PriorityQueue and operations

--- Collections/src/priorityQueue.java
+++ Collections/src/priorityQueue.java
@@ -4,7 +4,7 @@ public class priorityQueue {
         PriorityQueue<String> pqueue=new PriorityQueue<>();//similar to queue but difference in priority
         pqueue.add("Parks");
         pqueue.add("Aiden");
-        pqueue.add("Sam");//priority queue stores elements in an order ie alphabetical or numerical etc.
+        pqueue.add("Sam");//priority queue stores elements in order ie alphabetical or numerical etc.
         System.out.println(pqueue);
         System.out.println(pqueue.peek());//peek method also works in ordered manner
     }
