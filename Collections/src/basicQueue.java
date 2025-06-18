504372973c0faddd9d7c78df6c25c3666bb79dae
Dreamless07 <saadkhan7903@gmail.com>
2025-06-18 10:24:41 +0530

    using Queue interface by using inbuilt class as Linked List and operations

---
+++ Collections/src/basicQueue.java
@@ -0,0 +1,15 @@
+import java.util.LinkedList;
+import java.util.Queue;//inbuilt
+public class basicQueue {
+    public static void main(String[] args) {
+        Queue<String> newQ=new LinkedList<>();//can use 3 inbuilt classes ll, arraydeque and priority queue
+        newQ.offer("Jim");//insert
+        newQ.offer("Jack");
+        newQ.offer("Raze");
+        System.out.println(newQ);
+        newQ.peek();//read data
+        System.out.println(newQ);
+        newQ.poll();//remove first element
+        System.out.println(newQ);
+    }
+}
