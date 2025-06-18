9b2c3760278e7ddd744a6de074b502fa394661fd
Dreamless07 <saadkhan7903@gmail.com>
2025-06-18 10:51:39 +0530

    using Queue interface by using inbuilt class as Linked List and operations

---
+++ Collections/src/priorityQueue.java
@@ -0,0 +1,11 @@
+import java.util.PriorityQueue;
+public class priorityQueue {
+    public static void main(String[] args) {
+        PriorityQueue<String> pqueue=new PriorityQueue<>();//similar to queue but difference in priority
+        pqueue.add("Parks");
+        pqueue.add("Aiden");
+        pqueue.add("Sam");//priority queue stores elements in an order ie alphabetical or numerical etc.
+        System.out.println(pqueue);
+        System.out.println(pqueue.peek());//peek method also works in ordered manner
+    }
+}
