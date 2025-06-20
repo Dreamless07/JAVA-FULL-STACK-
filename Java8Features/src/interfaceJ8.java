333fc944b50092e0ae552584a5a5f0d44de03a0b
Dreamless07 <saadkhan7903@gmail.com>
2025-06-20 10:39:49 +0530

    define a default method in interface

--- Java8Features/src/interfaceJ8.java
+++ Java8Features/src/interfaceJ8.java
@@ -1,14 +1,14 @@
-interface newinter{//interface
+interface newInter {//interface
     default void newMeth() {//method using default we can define it in interface itself
         System.out.println("We can use method body in Interface by using Default keyword in Java 8");
     }
 }
-class newClass implements newinter{//class
+class newClass implements newInter {//class
 }
 
 public class interfaceJ8 {
     public static void main(String[] args) {
-        newinter newOb=new newClass();//object for that class
+        newInter newOb=new newClass();//object for that class
         newOb.newMeth();//call the method
     }
 }
