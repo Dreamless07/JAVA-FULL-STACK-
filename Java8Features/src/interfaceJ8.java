cf0333aa885d4579cd4d4438264629a1f95ae155
Dreamless07 <saadkhan7903@gmail.com>
2025-06-20 10:46:06 +0530

    define a default method and static method in interface

--- Java8Features/src/interfaceJ8.java
+++ Java8Features/src/interfaceJ8.java
@@ -1,3 +1,5 @@
+//                          with default keyword
+
 interface newInter {//interface
     default void newMeth() {//method using default we can define it in interface itself
         System.out.println("We can use method body in Interface by using Default keyword in Java 8");
@@ -12,3 +14,20 @@ public class interfaceJ8 {
         newOb.newMeth();//call the method
     }
 }
+
+/*                           with static keyword
+
+interface newInter {//interface
+    static void newMeth() {//method using static we can define it in interface itself
+        System.out.println("We can use method body in Interface by using Default keyword in Java 8");
+    }
+}
+class newClass implements newInter {//class
+}
+
+public class interfaceJ8 {
+    public static void main(String[] args) {
+        newInter.newMeth();//call the method directly through Interface because no object needed
+    }
+}
+ */
