857a0f1ac1498cabb16afe0057fb72624f2d044b
Dreamless07 <saadkhan7903@gmail.com>
2025-06-23 11:02:07 +0530

    functional interface feature and usage of for each loop and then updated with lambda function.

--- Java8Features/src/functionalInterface.java
+++ Java8Features/src/functionalInterface.java
@@ -2,16 +2,21 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;//inbuilt libraries
 
+
+/*
 class example1 implements Consumer<Integer>{//Consumer is a Functional Interface and lambda function can also be used
     public void accept(Integer i){//this Interface contains a default method accept
-        System.out.println(i);
+        System.out.println(i);                                                      // while using normal class definition
     }
 }
 
+
+*/
 public class functionalInterface {
     public static void main(String[] args) {
         List<Integer> listOb = Arrays.asList(1,2,3,4,5);//inbuilt Arrays method can use asList directly
-        Consumer<Integer> obj=new example1();//Consumer object creation
+        //Consumer<Integer> obj=new example1();//Consumer object creation
+        Consumer<Integer>obj=i->System.out.println(i);                       //using lambda function
         listOb.forEach(obj);//call object from for each method to accept method
     }
 }
