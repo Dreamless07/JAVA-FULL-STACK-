9420ad7a7b7ce25cfcf1532a2f0ec80d32591990
Dreamless07 <saadkhan7903@gmail.com>
2025-06-24 10:51:38 +0530

    stream api: used predicate in Filter and then used lambda function to reduce it.

--- Java8Features/src/streamApi.java
+++ Java8Features/src/streamApi.java
@@ -1,13 +1,13 @@
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Stream;//inbuilt libraries
+import java.util.function.Predicate;
 
 public class streamApi {
     public static void main(String[] args) {
-        List<Integer> n1 = Arrays.asList(1,2,3,4,5,6);//stores elements in nl
+        List<Integer> n1 = Arrays.asList(1, 2, 3, 4, 5, 6);//stores elements in n1 directly as list
         System.out.println(n1);
-        //predicate is also a functional interface(inbuilt)
-        Stream s1= n1.stream();//Stream is both Interface & method, can be used only once, converts n1 into stream and stores in s1
-        s1.forEach(System.out::println);//method reference for : for-each method(can print like this)
+        n1.stream().filter((Integer i) -> i % 2 == 0).forEach(System.out::println);//predicate is also a functional interface(inbuilt) in Filter
+        //Stream is both Interface & method, can be used only once, converts n1 into stream and stores in s1
+        //method reference for : for-each method(can print like this)
     }
 }
