33a4b458c5ed5a5c07b71f1db5d9c437f061c9c0
Dreamless07 <saadkhan7903@gmail.com>
2025-06-24 11:29:22 +0530

    stream api: used predicate in Filter and then used lambda function to reduce it, then done same for Map and Reduce

--- Java8Features/src/streamApi.java
+++ Java8Features/src/streamApi.java
@@ -1,13 +1,17 @@
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.Predicate;
-
+import java.util.function.Function;//for map only when using without lambda
+import java.util.function.Predicate;//for filter only when using without lambda
+import java.util.function.BinaryOperator;//for reduce only when using without lambda
 public class streamApi {
     public static void main(String[] args) {
         List<Integer> n1 = Arrays.asList(1, 2, 3, 4, 5, 6);//stores elements in n1 directly as list
-        System.out.println(n1);
-        n1.stream().filter((Integer i) -> i % 2 == 0).forEach(System.out::println);//predicate is also a functional interface(inbuilt) in Filter
-        //Stream is both Interface & method, can be used only once, converts n1 into stream and stores in s1
-        //method reference for : for-each method(can print like this)
+        //Stream is both Interface & method, can be used only once, converts n1 into stream
+        System.out.println(n1);//we have used lambda function for all below
+        Integer sum=n1.stream().filter(i -> i % 2 == 0)//Predicate is functional interface, inbuilt method is Test in Filter
+                               .map(i -> i*2)//Function is functional interface, inbuilt method is Apply in Map
+                               .reduce(0,(i,j)->i+j);//Binary Operator is FI, inbuilt method is Apply in Reduce
+        System.out.println(sum);
+        //can use forEach(System.out::println) method reference only if many iterations
     }
 }
